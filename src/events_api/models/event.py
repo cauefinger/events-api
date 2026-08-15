32dfa762c09196362cf37ae8b0148d8381ad8af7
Caue Finger <fingercaue@gmail.com>
2026-08-14 23:52:05 -0300

    create enum for status

--- src/events_api/models/event.py
+++ src/events_api/models/event.py
@@ -1,8 +1,13 @@
 
-
-
 from ..database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, SQLAlchemyEnum
+from enum import Enum
+
+class StatusEvent(Enum):
+    PENDING = "PENDING"
+    ACTIVE = "ACTIVE"
+    CANCELED = "CANCELED"
+
 
 class Event(Base):
     __tablename__ = "event"
@@ -13,15 +18,18 @@ class Event(Base):
     location = Column(String, unique=True, index=True)
     date = Column(Integer, primary_key=True, index=True)
     capacity = Column(Integer, unique=True, index=True)
-    status = Column(String, unique=True, index=True)
+    status = Column(
+        SQLAlchemyEnum(StatusEvent),
+          default=StatusEvent.PENDING
+          )
 
 
-    def __init__(self, id:Integer, title: String, description: String, location: String, date: Integer, capacity:Integer, status: String):
+    def __init__(self, id:Integer, title: String, description: String, location: String, date: Integer, capacity:Integer, status: StatusEvent):
         self.id = id
         self.title = title
         self.description = description
         self.location = location
-        self.date = date
+        self.date = date    
         self.capacity = capacity
-        self.status = status
+        self.status = StatusEvent
     
