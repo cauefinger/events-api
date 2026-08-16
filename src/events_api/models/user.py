6a822c874ed7c410118736286a2a796bfa40749e
Caue Finger <fingercaue@gmail.com>
2026-08-15 23:47:42 -0300

    Changes added regarding the User class.

--- src/events_api/models/user.py
+++ src/events_api/models/user.py
@@ -7,13 +7,7 @@ class User(Base):
     __tablename__ = "users"
 
     #TODO: Fix unique 
-    id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, unique=True, index=True)
-    last_name = Column(String, unique=True, index=True)
+    id = Column(Integer,unique=True, primary_key=True, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
-
-    def __init__(self, first_name: str, last_name: str, email: str):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-    
