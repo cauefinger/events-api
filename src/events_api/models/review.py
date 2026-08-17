c95a45f4966176eae136b949d0fab7f0532d90d5
Caue Finger <fingercaue@gmail.com>
2026-08-17 10:53:17 -0300

    add foreign key to ticket, event

--- src/events_api/models/review.py
+++ src/events_api/models/review.py
@@ -2,15 +2,15 @@ import uuid
 from ..database import Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import ForeignKey
 
-# TODO: usar foreignkey no user id e event id
  
 class Review(Base):
     __tablename__ = "review"
 
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
-    eventId = Column(UUID(as_uuid=True), index=True)
-    userId = Column(UUID(as_uuid=True), index=True)
+    eventId = Column(UUID(as_uuid=True), ForeignKey("event.id"), index=True)
+    userId = Column(UUID(as_uuid=True),ForeignKey("user.id"),index=True)
     rating = Column(Integer, index=True)
     comment = Column(String, index=True)
    
--- src/events_api/models/ticket.py
+++ src/events_api/models/ticket.py
@@ -1,22 +1,18 @@
 import uuid
 from ..database import Base
-from sqlalchemy import Column, Integer, String, Float, Enum
+from sqlalchemy import Column, Integer, String, Float, Enum, ForeignKey
 from ..enums.status_ticket import Status_Ticket
 from sqlalchemy.dialects.postgresql import UUID
 
-# TODO: usar foreignkey no user id, event id
 
 
 class Ticket(Base):
     __tablename__ = "ticket"
 
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
-    eventId = Column(UUID(as_uuid=True),index=True)
-    userId = Column(UUID(as_uuid=True), index=True)
+    eventId = Column(UUID(as_uuid=True),ForeignKey("event.id"), index=True)
+    userId = Column(UUID(as_uuid=True),ForeignKey("user.id"), index=True)
     type = Column(String, index=True)
     price = Column(Integer, index=True)
-    status = Column(
-        Enum(Status_Ticket),
-          default=Status_Ticket.PENDING
-          )
+    status = Column(Enum(Status_Ticket), default=Status_Ticket.PENDING)
 
