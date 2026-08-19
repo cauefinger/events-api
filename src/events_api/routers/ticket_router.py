10a57e505c80afcab0d6db878f1278788f8d25f4
Caue Finger <fingercaue@gmail.com>
2026-08-19 17:30:03 -0300

    Ticket purchase route created.

--- src/events_api/routers/ticket_router.py
+++ src/events_api/routers/ticket_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from events_api.dependencies.get_session import get_session
 from events_api.models.ticket import Ticket
@@ -10,3 +10,16 @@ async def visualizar_tickets(session: Session = Depends(get_session)):
     quantidade = session.query(Ticket).filter(Ticket.buyer_id.is_(None)).count()
     return {"tickets_disponiveis": quantidade}
 
+
+@ticket_router.post("/comprar/{ticket_id}")
+async def comprar_tickets(ticket_id: int, session: Session = Depends(get_session)):
+
+    ticket = session.query(Ticket).filter(
+        Ticket.id == ticket_id
+    ).first()
+
+    if ticket.buyer_id is not None:
+        raise HTTPException(
+            status_code=400,
+            detail = ("Ticket indisponível.")
+        )
